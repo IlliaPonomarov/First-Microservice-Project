4f04115b68564600b61a667b9f7bce35e858202a
Illia <chippaxyucac@gmail.com>
2022-12-27 17:32:56 +0100

    docker compose was configurated for cards microservice.

--- cards/src/main/java/com/micro/cards/controllers/CardController.java
+++ cards/src/main/java/com/micro/cards/controllers/CardController.java
@@ -3,8 +3,11 @@ package com.micro.cards.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -48,10 +51,17 @@ public class CardController {
 
         if (cards.isEmpty())
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-
-
-
         
         return new ResponseEntity<>(cards, HttpStatus.OK);
+    }
+
+
+    @PostMapping("/")
+    public ResponseEntity<Card> save(@RequestBody Card card, BindingResult result) {
+
+        if (result.hasErrors())
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        return new ResponseEntity<>(cardService.save(card), HttpStatus.CREATED);
     }
 }
